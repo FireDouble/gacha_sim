43a18e2cb63082bdeb6e5f7968da530e75936b08
FireDouble <75065640+FireDouble@users.noreply.github.com>
2025-02-05 15:44:36 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in rec {
+        packages = {
+          frontend = pkgs.buildNpmPackage {
+            pname = "frontend";
+            version = "0.1.0";
+
+            src = ./frontend;
+            npmDepsHash = "sha256-tIiX0qfnQIaLfc/LGj8QOwmLcGykmWmzXZsQtLKZ/eo=";
+
+            nativeBuildInputs = with pkgs; [
+              tailwindcss
+            ];
+
+            npmBuildScript = "build";
+          };
+          backend = pkgs.rustPlatform.buildRustPackage {
+            pname = "backend";
+            version = "0.1.0";
+
+            src = ./backend;
+
+            cargoLock = {
+              lockFile = ./backend/Cargo.lock;
+            };
+
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+              makeWrapper
+            ];
+          };
+
+          default = self.packages.${system}.frontend;
+        };
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [packages.default];
+
+          RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+          packages = with pkgs; [
+            clippy
+            rustup
+
+            tailwindcss
+            nodejs_23
+          ];
+        };
+      }
+    );
+}
---
+++ frontend/src/main.js
@@ -0,0 +1,10 @@
+import { mount } from 'svelte'
+import './tailwind.css'
+import './globals.css'
+import App from './App.svelte'
+
+const app = mount(App, {
+  target: document.getElementById('app'),
+})
+
+export default app
